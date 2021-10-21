2d88c4be31afac700184677fb9866851df7d5de1
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-10-21 19:34:59 +0200

    Add Seeder

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $post = new Post();
+
+            $post->title = $faker->text(50);
+            $post->content = $faker->text();
+            $post->image = $faker->imageUrl(250, 250);
+            $post->slug = Str::slug($post->title, '-');
+
+            $post->save();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Auth::routes(['register' => false]);
+Auth::routes(['register' => true]);
 
 Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function () {
     // tutte le rotte protette
